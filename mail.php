f5b3cd895f6f3fc1617a486ef7313bd30623b425
Nicolas PEROT <Nicolas.PEROT@etu.uca.fr>
2020-11-07 01:11:20 +0100

    Envoie de quelques modifications

--- mail.php
+++ mail.php
@@ -7,17 +7,19 @@
 
 <body>
 <?php
-$entete  = 'MIME-Version: 1.0' . "\r\n";
-$entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-$entete .= 'From: ' . $_POST['email'] . "\r\n";
+if(isset($_POST['message'])){
+    $entete  = 'MIME-Version: 1.0' . "\r\n";
+    $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+    $entete .= 'From: ' . $_POST['email'] . "\r\n";
 
-$message = '<h1>Message envoyé depuis la page Contact de monsite.fr</h1>
-        <p><b>Nom : </b>' . $_POST['nom'] . ' ' . $_POST['prenom'] . ' <br>
-        <b>Email : </b>' . $_POST['email'] . '<br>
-        <b>Message : </b>' . $_POST['message'] . '</p>';
-$retour = mail('nico.perot@hotmail.fr', 'Envoi depuis la page Contact', $message, $entete );
-if ($retour) {
-    echo '<p>Merci ! Votre message a bien été envoyé.</p>';
+    $message = '<h1>Message envoyé depuis la page Contact de monsite.fr</h1>
+            <p><b>Nom : </b>' . $_POST['nom'] . ' ' . $_POST['prenom'] . ' <br>
+            <b>Email : </b>' . $_POST['email'] . '<br>
+            <b>Message : </b>' . $_POST['message'] . '</p>';
+    $retour = mail('nico.perot@hotmail.fr', 'Envoi depuis la page Contact', $message, $entete );
+    if ($retour) {
+        echo '<p>Merci ! Votre message a bien été envoyé.</p>';
+    }
 }
 ?>
 </body>
